0e59bcc7586fdab97c622db87b54abda1f3414eb
Rico McCoy <68340447+rmccoy2k@users.noreply.github.com>
2021-05-15 13:31:47 -0500

    Add files via upload

---
+++ Export SQL Data to a Flat File.sql
@@ -0,0 +1,35 @@
+         /****************************************************************
+				          ****EXPORT SQL DATA TO A FLAT FILE****
+         ****************************************************************/
+		 
+		               --===================================
+		               --USE the SSISPROJECTS Database
+					   --===================================
+
+
+CREATE DATABASE SSISProjects
+GO
+
+USE SSISProjects
+GO
+
+CREATE TABLE Mountains
+(
+	Mountainid INT,
+	MountainName VARCHAR (50),
+	MountainHeight VARCHAR (15)
+)
+
+INSERT INTO 
+	Mountains (Mountainid, MountainName, MountainHeight)
+VALUES 
+	(1, 'Everest', '29,035'),
+	(2, 'Karakoram', '28,250'),
+	(3, 'Kanchenjunga', '28,169')
+
+
+SELECT
+	*
+FROM
+	Mountains /*<<Highlight the table name and then press Alt + F1 
+	to view data types of fields.*/
